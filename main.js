e10116642ddbc838f9f02b561a621587de8ba310
Francisco Diaz <franciscodiazhuerta97@gmail.com>
2023-10-09 14:52:59 -0600

    se agregaron mas estilos css y se empieza a trabajar js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 const ApiKey = "19631b889c86a9c043c45a2f5433a93a"
+
+const form = document.querySelector(".top-banner form");
+ form.addEventListener("submit", e =>
+  { e.preventDefault(); 
+    const inputVal = input.value; 
+ });
+  
